a7b9af34f847b6cf85a0edd2714d03a1ba01072e
Frances Thai <notfelineit@gmail.com>
2020-04-04 22:50:24 -0700

    Removing block .ts files since we're adding /api

---
+++ api/_google-sheet.ts
@@ -0,0 +1,46 @@
+
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { v4 as uuid } from 'uuid'
+
+const CLIENT_EMAIL = process.env.CLIENT_EMAIL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const SHEET_ID = process.env.SHEET_ID
+
+console.log(process.env)
+
+export interface FormEntry {
+  email: string
+  summary: string
+  state: string
+  county: string
+  action: string
+  date: string
+  source: string
+}
+
+class GoogleSheet {
+  sheet: GoogleSpreadsheet
+  initialized: boolean
+
+  constructor() {
+    this.sheet = new GoogleSpreadsheet(SHEET_ID)
+  }
+
+  async init() {
+    await this.sheet.useServiceAccountAuth({
+      client_email: CLIENT_EMAIL,
+      private_key: PRIVATE_KEY,
+    })
+
+    this.initialized = true
+  }
+
+  async addRow(entry: FormEntry) {
+    const row = [uuid(), entry.email, entry.summary, entry.state, entry.county, entry.action, entry.date, entry.source]
+    await this.sheet.addRow(row)
+  }
+}
+
+export default new GoogleSheet()
+
+
---
+++ api/submit-form.ts
@@ -0,0 +1,21 @@
+import { NowRequest, NowResponse } from '@now/node'
+import googleSheet, { FormEntry } from './_google-sheet'
+
+module.exports = async (req: NowRequest, res: NowResponse) => {
+  if (!googleSheet.initialized) {
+    await googleSheet.init()
+  }
+
+  // await googleSheet.addRow(req.body as FormEntry)
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies
+  })
+}
+
+
+
+
+
+
