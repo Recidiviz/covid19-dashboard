2a592edf4f42dd402a15a7a0c58a659feec483f7
Sean Zhu <interestinglythere@gmail.com>
2020-04-07 11:28:58 -0700

    Revert "Make a change that causes an eslint warning"
    This reverts commit 1408b92887302e81c2c89c93ecb9b99d5c9c8eb3.

--- src/entry-point-client/App.tsx
+++ src/entry-point-client/App.tsx
@@ -5,8 +5,6 @@ import { GlobalStyles } from "../styles";
 import PageList from "./PageList";
 import WindowTitle from "./WindowTitle";
 
-const x = 1;
-
 const App: React.FC = () => {
   return (
     <>
