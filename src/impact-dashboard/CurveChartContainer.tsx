1646f133cf81d52449681ed88394653f8fb3a227
Ian MacFarland <ian@recidiviz.org>
2020-04-07 19:54:34 -0700

    output base incarcerated and staff curves from infection-model

--- src/impact-dashboard/CurveChartContainer.tsx
+++ src/impact-dashboard/CurveChartContainer.tsx
@@ -1,4 +1,8 @@
-import { calculateCurves } from "../infection-model";
+import { zip } from "d3-array";
+
+import { calculateCurves, CurveData } from "../infection-model";
+import { getAllValues, getColView } from "../infection-model/matrixUtils";
+import { seirIndex } from "../infection-model/seir";
 import { MarkColors } from "./ChartArea";
 import CurveChart from "./CurveChart";
 import { useEpidemicModelState } from "./EpidemicModelContext";
@@ -7,20 +11,37 @@ interface Props {
   markColors: MarkColors;
 }
 
+// for these curves we combine incarcerated and staff
+function combinePopulations(data: CurveData, columnIndex: number) {
+  return zip(
+    getAllValues(getColView(data.incarcerated, columnIndex)),
+    getAllValues(getColView(data.staff, columnIndex)),
+  ).map(([incarcerated, staff]) => incarcerated + staff);
+}
+
 const CurveChartContainer: React.FC<Props> = ({ markColors }) => {
   const modelData = useEpidemicModelState();
+  // TODO: could this be stored on the context instead for reuse?
+  const projectionData = calculateCurves(modelData);
+  // merge and filter the curve data to only what we need for the chart
+  const curveData = {
+    exposed: combinePopulations(projectionData, seirIndex.exposed),
+    fatalities: combinePopulations(projectionData, seirIndex.fatalities),
+    hospitalized: combinePopulations(projectionData, seirIndex.hospitalized),
+    infectious: combinePopulations(projectionData, seirIndex.infectious),
+  };
 
   return modelData.countyLevelDataLoading ? (
     <div>Loading...</div>
   ) : (
     <CurveChart
-      curveData={calculateCurves(modelData)}
       hospitalBeds={
         modelData.countyLevelData
           ?.get(modelData.stateCode)
           // we don't show this until data is loaded so nothing should be undefined
           ?.get(modelData.countyName as string)?.hospitalBeds as number
       }
+      curveData={curveData}
       markColors={markColors}
     />
   );
