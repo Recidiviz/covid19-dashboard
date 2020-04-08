9c94a48d5d935faa333e29d0d6bd3f9b81b41721
Ian MacFarland <ian@recidiviz.org>
2020-04-07 22:32:34 -0700

    update logo (closes #36)

--- src/site-header/SiteHeader.tsx
+++ src/site-header/SiteHeader.tsx
@@ -1,6 +1,24 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import { useAuth0 } from "../auth/react-auth0-spa";
+import Colors from "../design-system/Colors";
+import Logo from "./Logo";
+
+const LogoContainer = styled.div`
+  align-items: flex-end;
+  color: ${Colors.red};
+  display: flex;
+  font-family: "Poppins", sans-serif;
+  font-size: 13px;
+  font-weight: 600;
+  letter-spacing: -0.05em;
+  text-transform: uppercase;
+`;
+
+const Subhead = styled.div`
+  margin-left: 1em;
+`;
 
 const SiteHeader: React.FC = () => {
   const { isAuthenticated, loginWithRedirect, logout } = (useAuth0 as any)();
@@ -9,17 +27,13 @@ const SiteHeader: React.FC = () => {
 
   return (
     <nav className="font-semibold font-display text-sm">
-      <div className="flex justify-between h-16 flex-wrap">
-        <div className="flex">
-          <Link to="/" className="flex uppercase">
-            <h3 className="inline-flex items-center px-1 pt-1 leading-5">
-              CJ Status •
-            </h3>
-            <h3 className="inline-flex items-center px-1 pt-1 leading-5 text-red">
-              COVID-19 Incarceration Model
-            </h3>
-          </Link>
-        </div>
+      <div className="flex justify-between h-16 flex-wrap items-center">
+        <Link to="/">
+          <LogoContainer>
+            <Logo />
+            <Subhead>COVID-19 Incarceration Model</Subhead>
+          </LogoContainer>
+        </Link>
         {/* <!-- Nav Items and Social Links --> */}
         <div className="flex items-center justify-between">
           <Link
