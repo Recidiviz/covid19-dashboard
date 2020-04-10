0cfc90c78bdbc55e55c30e2f274862abdd567d23
Sean Zhu <interestinglythere@gmail.com>
2020-04-10 11:08:59 -0700

    Update copy

--- src/page-unsupported-browser/UnsupportedBrowserPage.tsx
+++ src/page-unsupported-browser/UnsupportedBrowserPage.tsx
@@ -12,19 +12,21 @@ const UnsupportedBrowserPage: React.FC = () => (
         <main className="py-8 sm:py-16 sm:py-24">
           <div className="max-w-3xl mx-auto px-6 sm:px-8 lg:px-10 flex flex-col justify-start">
             <p className="leading-7 text-base sm:text-lg my-6">
-              Sorry, you're using a legacy browser that that site does not
-              support.
+              We’re sorry, legacy browsers such as Internet Explorer are not
+              currently supported by the COVID-19 model. Please try loading the
+              website in a modern browser such as Microsoft Edge, Google Chrome,
+              Firefox, or Safari.
             </p>
             <p className="leading-7 text-base sm:text-lg my-6">
-              Please use Google Chrome, Firefox, Microsoft Edge, or any other
-              modern browser.
-            </p>
-            <p className="leading-7 text-base sm:text-lg my-6">
-              If you have further issues, please email us at{" "}
-              <a href="mailto:covid@recidiviz.org" className="font-semibold">
+              If you think you are seeing this message in error, or if you can’t
+              switch browsers, please email us at{" "}
+              <a
+                href="mailto:covid@recidiviz.org?Subject=COVID%20Model%20Browser%20Support"
+                className="font-semibold"
+              >
                 covid@recidiviz.org
-              </a>
-              .
+              </a>{" "}
+              so we can provide support.
             </p>
           </div>
         </main>
