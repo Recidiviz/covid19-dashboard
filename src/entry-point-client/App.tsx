45330ce83a669ec43942fa06fd1709f464daf531
Sean Zhu <interestinglythere@gmail.com>
2020-04-09 20:52:54 -0700

    Move VerificationNeeded into PageList

--- src/entry-point-client/App.tsx
+++ src/entry-point-client/App.tsx
@@ -4,7 +4,6 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import PrivateRoute from "../auth/PrivateRoute";
 import { useAuth0 } from "../auth/react-auth0-spa";
 import Loading from "../design-system/Loading";
-import VerificationNeeded from "../page-verification-needed/VerificationNeeded";
 import { GlobalStyles } from "../styles";
 import PageList from "./PageList";
 import WindowTitle from "./WindowTitle";
@@ -27,11 +26,6 @@ const App: React.FC = () => {
     <>
       <GlobalStyles />
       <Switch>
-        <Route path="/verify">
-          <WindowTitle>Verification Needed</WindowTitle>
-          <VerificationNeeded />
-        </Route>
-
         {PageList.map(({ path, title, isPrivate, contents }) => {
           if (!isPrivate) {
             return (
