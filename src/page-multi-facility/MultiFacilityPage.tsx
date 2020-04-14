418bf5b33d7f68001c4f2ba32dfdd1fbed85433f
Daniela Schiano di Cola <daschi87@gmail.com>
2020-04-14 14:21:43 -0700

    Add the InputToggle component and styles, preview on MultiFacilityPage

--- src/page-multi-facility/MultiFacilityPage.tsx
+++ src/page-multi-facility/MultiFacilityPage.tsx
@@ -1,16 +1,24 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
+import InputToggle from "../design-system/InputToggle";
 import SiteHeader from "../site-header/SiteHeader";
 
 const MultiFacilityPageDiv = styled.div``;
 
 const MultiFacilityPage: React.FC = () => {
+  const [toggled, setToggled] = useState(false);
+
   return (
     <MultiFacilityPageDiv>
       <div className="font-body text-green min-h-screen tracking-normal w-full">
         <div className="max-w-screen-xl px-4 mx-auto">
           <SiteHeader />
           <main className="my-6">
+            <InputToggle
+              toggled={toggled}
+              onChange={() => setToggled(!toggled)}
+            />
           </main>
         </div>
       </div>
