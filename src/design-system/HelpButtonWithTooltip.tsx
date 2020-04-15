d5032ce3ec58d436d6aad96324a40b7d5de24918
Jen <jovergaag@gmail.com>
2020-04-14 18:10:57 -0700

    HelpButtonWithTooltip fix props

--- src/design-system/HelpButtonWithTooltip.tsx
+++ src/design-system/HelpButtonWithTooltip.tsx
@@ -7,23 +7,23 @@ const HelpButton = styled.span<{ muted?: boolean }>`
   background: ${(props) =>
     props.muted ? `${Colors.darkTeal};` : `${Colors.forest};`}
   color: white;
-  border-radius: ${(props) => (props.muted ? "9px;" : "14px")}
-  line-height: ${(props) => (props.muted ? "7px;" : "12px")}
-  width: ${(props) => (props.muted ? "9px;" : "16px")}
-  height: ${(props) => (props.muted ? "9px;" : "16px")}
+  border-radius: ${(props) => (props.muted ? "9px;" : "14px;")}
+  line-height: ${(props) => (props.muted ? "7px;" : "12px;")}
+  width: ${(props) => (props.muted ? "9px;" : "16px;")}
+  height: ${(props) => (props.muted ? "9px;" : "16px;")}
   display: inline-block;
   text-align: center;
-  font-size: ${(props) => (props.muted ? "6px;" : "9px")}
+  font-size: ${(props) => (props.muted ? "6px;" : "9px;")}
   align-self: center;
   padding: 2px;
 `;
 
 interface Props {
-  muted?: React.ReactNode;
   children: React.ReactNode;
+  muted?: boolean;
 }
 
-const HelpButtonWithTooltip: React.FC = (props) => {
+const HelpButtonWithTooltip: React.FC<Props> = (props) => {
   return (
     <Tooltip content={props.children}>
       <HelpButton muted={props.muted}>?</HelpButton>
