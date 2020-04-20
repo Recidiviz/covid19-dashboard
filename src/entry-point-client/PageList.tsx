6e520e9026d4d19b14d7de42e5188f6d37a6d9c8
Ian MacFarland <macfarlandian@users.noreply.github.com>
2020-04-19 17:17:35 -0700

    move multi-facility page to site home (#149)

--- src/entry-point-client/PageList.tsx
+++ src/entry-point-client/PageList.tsx
@@ -2,7 +2,6 @@ import GetInvolvedPage from "../page-get-involved/GetInvolvedPage";
 import ModelInspectionPage from "../page-model-inspection/";
 import FacilityPage from "../page-multi-facility/FacilityPage";
 import MultiFacilityPage from "../page-multi-facility/MultiFacilityPage";
-import OverviewPage from "../page-overview/OverviewPage";
 import UnsupportedBrowserPage from "../page-unsupported-browser/UnsupportedBrowserPage";
 import VerificationNeeded from "../page-verification-needed/VerificationNeeded";
 
@@ -22,7 +21,7 @@ const PageList: PageInfo[] = [
     path: "/",
     title: getPageTitle(),
     isPrivate: true,
-    contents: <OverviewPage />,
+    contents: <MultiFacilityPage />,
   },
   {
     path: "/get-involved",
@@ -43,13 +42,7 @@ const PageList: PageInfo[] = [
     contents: <VerificationNeeded />,
   },
   {
-    path: "/multi-facility",
-    title: getPageTitle(),
-    isPrivate: true,
-    contents: <MultiFacilityPage />,
-  },
-  {
-    path: "/multi-facility/facility",
+    path: "/facility",
     title: getPageTitle(),
     isPrivate: true,
     contents: <FacilityPage />,
