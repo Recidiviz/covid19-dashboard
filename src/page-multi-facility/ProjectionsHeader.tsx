a8d7da6bd823e5825d6a18ac5abe10e44e5d54ef
Joshua Essex <essexjosh@gmail.com>
2020-04-19 23:31:04 -0600

    Adds circles with proper colors for the projection headers above the facility rows in the scenario details page.

--- src/page-multi-facility/ProjectionsHeader.tsx
+++ src/page-multi-facility/ProjectionsHeader.tsx
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+
+import { MarkColors as markColors } from "../design-system/Colors";
+
+const LegendCircle = styled.span`
+  color: ${(props) => props.color};
+  font-size: 18px;
+`;
+
 const ProjectionsHeader: React.FC = () => {
   return (
     <div className="border-t border-b border-gray-300 mt-5 mb-5 py-2 flex flex-row">
@@ -8,10 +17,18 @@ const ProjectionsHeader: React.FC = () => {
       <div className="w-3/5 flex flex-row justify-between">
         <div>Projection</div>
         <div>
-          <span className="ml-6">Fatalities</span>
-          <span className="ml-6">Exposed</span>
-          <span className="ml-6">Infectious</span>
-          <span className="ml-6">Hospitalized</span>
+          <span className="ml-6">
+            <LegendCircle color={markColors.fatalities}>•</LegendCircle> Fatalities
+          </span>
+          <span className="ml-6">
+            <LegendCircle color={markColors.exposed}>•</LegendCircle> Exposed
+          </span>
+          <span className="ml-6">
+            <LegendCircle color={markColors.infectious}>•</LegendCircle> Infectious
+          </span>
+          <span className="ml-6">
+            <LegendCircle color={markColors.hospitalized}>•</LegendCircle> Hospitalized
+          </span>
         </div>
       </div>
     </div>
