a89a7823814385cffa87eb4eb32696e349e8c889
Colin Folta <colin.folta@gmail.com>
2020-04-22 16:31:01 -0500

    graph headers: shrink font, change color, replace bullet with circle div

--- src/page-multi-facility/ProjectionsHeader.tsx
+++ src/page-multi-facility/ProjectionsHeader.tsx
@@ -1,38 +1,53 @@
 import styled from "styled-components";
 
-import { MarkColors as markColors } from "../design-system/Colors";
+import Colors, { MarkColors as markColors } from "../design-system/Colors";
 
-const LegendCircle = styled.span`
-  color: ${(props) => props.color};
-  font-size: 18px;
+const LegendItem = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LegendCircle = styled.div`
+  background-color: ${(props) => props.color};
+  width: 5px;
+  height: 5px;
+  border-radius: 50%;
+  margin-right: 4px;
+`;
+
+const LegendText = styled.div`
+  color: ${Colors.forest};
+  opacity: 0.7;
+  font-family: "Poppins", sans-serif;
+  font-size: 9px;
+  font-weight: 600;
+  line-height: 16px;
 `;
 
 const ProjectionsHeader: React.FC = () => {
   return (
     <div className="border-t border-b border-gray-300 mt-5 mb-5 py-2 flex flex-row">
       <div className="w-2/5 flex flex-row">
-        <div className="w-1/4">Cases</div>
-        <div className="w-3/4">Facility</div>
+        <LegendText className="w-1/4">Cases</LegendText>
+        <LegendText className="w-3/4">Facility</LegendText>
       </div>
       <div className="w-3/5 flex flex-row justify-between">
-        <div>Projection</div>
-        <div>
-          <span className="ml-6">
-            <LegendCircle color={markColors.fatalities}>•</LegendCircle>{" "}
-            Fatalities
-          </span>
-          <span className="ml-6">
-            <LegendCircle color={markColors.exposed}>•</LegendCircle> Exposed
-          </span>
-          <span className="ml-6">
-            <LegendCircle color={markColors.infectious}>•</LegendCircle>{" "}
-            Infectious
-          </span>
-          <span className="ml-6">
-            <LegendCircle color={markColors.hospitalized}>•</LegendCircle>{" "}
-            Hospitalized
-          </span>
-        </div>
+        <LegendItem className="ml-6">
+          <LegendCircle color={markColors.fatalities} />
+          <LegendText>Fatalities</LegendText>
+        </LegendItem>
+        <LegendItem className="ml-6">
+          <LegendCircle color={markColors.exposed} />
+          <LegendText>Exposed</LegendText>
+        </LegendItem>
+        <LegendItem className="ml-6">
+          <LegendCircle color={markColors.infectious} />
+          <LegendText>Infectious</LegendText>
+        </LegendItem>
+        <LegendItem className="ml-6">
+          <LegendCircle color={markColors.hospitalized} />
+          <LegendText>Hospitalized</LegendText>
+        </LegendItem>
       </div>
     </div>
   );
