114ca72be6443e49f68416ce6814ede106d17ee7
Ian MacFarland <macfarlandian@users.noreply.github.com>
2020-04-24 09:50:57 -0700

    Timeseries chart for R(t) (#207)

---
+++ src/rt-timeseries/RtTimeseries.tsx
@@ -0,0 +1,133 @@
+import { scaleTime, timeFormat } from "d3";
+// no type defs for Semiotic
+const ResponsiveXYFrame = require("semiotic/lib/ResponsiveXYFrame") as any;
+import styled from "styled-components";
+
+import ChartTooltip from "../design-system/ChartTooltip";
+import ChartWrapper from "../design-system/ChartWrapper";
+import Colors, { lighten } from "../design-system/Colors";
+
+export type Record = {
+  date: Date;
+  value: number;
+};
+
+interface Props {
+  data: {
+    Rt: Record[];
+    low90: Record[];
+    high90: Record[];
+  };
+}
+
+type Line = {
+  confidenceInterval?: boolean;
+  data: Record[];
+  title: string;
+};
+
+const formatDate = timeFormat("%-d %B");
+
+const RtTimeseriesWrapper = styled(ChartWrapper)`
+  .uncertainty {
+    fill: ${Colors.darkGray};
+    fill-opacity: 0.3;
+  }
+`;
+
+const TooltipContents = styled.div`
+  text-align: center;
+  white-space: nowrap;
+`;
+
+const TooltipLabel = styled.div``;
+const TooltipValue = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 4px;
+`;
+
+const Tooltip: React.FC<{
+  data: Record;
+  parentLine: { title: string };
+}> = (props) => {
+  const {
+    data: { date, value },
+    parentLine: { title },
+  } = props;
+  return (
+    <ChartTooltip>
+      <TooltipContents>
+        <TooltipValue>{value}</TooltipValue>
+        <TooltipLabel>{title}</TooltipLabel>
+        <TooltipLabel>{formatDate(date)}</TooltipLabel>
+      </TooltipContents>
+    </ChartTooltip>
+  );
+};
+
+const RtTimeseries: React.FC<Props> = ({ data }) => {
+  const getLines = (): Line[] => {
+    return [
+      { title: "R(t)", data: data.Rt },
+      {
+        title: "90% Confidence Interval (low)",
+        confidenceInterval: true,
+        data: data.low90,
+      },
+      {
+        title: "90% Confidence Interval (high)",
+        confidenceInterval: true,
+        data: data.high90,
+      },
+    ];
+  };
+
+  return (
+    <RtTimeseriesWrapper>
+      <ResponsiveXYFrame
+        annotations={[
+          {
+            type: "area",
+            className: "uncertainty",
+            coordinates: [
+              // assuming these are sorted in date order this makes a clean shape
+              ...data.high90,
+              ...[...data.low90].reverse(),
+            ],
+          },
+          {
+            color: lighten(Colors.red, 30),
+            disable: ["connector"],
+            type: "y",
+            value: 1,
+          },
+        ]}
+        axes={[
+          {
+            label: { name: "Rate of spread", locationDistance: 30 },
+            orient: "left",
+            tickLineGenerator: () => null,
+          },
+        ]}
+        hoverAnnotation
+        lineDataAccessor="data"
+        lines={getLines()}
+        lineStyle={(d: Line) => ({
+          stroke: Colors.forest,
+          strokeWidth: d.confidenceInterval ? 0 : 1,
+        })}
+        margin={{ left: 40, bottom: 30, right: 10, top: 10 }}
+        responsiveHeight
+        responsiveWidth
+        tooltipContent={Tooltip}
+        xAccessor="date"
+        xScaleType={scaleTime()}
+        yAccessor="value"
+        yExtent={[0]}
+      />
+    </RtTimeseriesWrapper>
+  );
+};
+
+export default RtTimeseries;
