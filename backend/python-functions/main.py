68fb618076c274a58b1d31ab1ada219ba927adae
Ian MacFarland <ian@recidiviz.org>
2020-04-24 18:25:39 -0700

    add CORS handler and schema validation

--- backend/python-functions/main.py
+++ backend/python-functions/main.py
@@ -1,42 +1,25 @@
 from collections import defaultdict
 from flask import json
 
-def calculate_rt(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request):
-            Expected JSON body:
-            {
-                "dates":[<(timestamp in s)>, ...],
-                "cases": [<number>, ...]
-            }
+from helpers import cloudfunction
+from schemas import rt_input, rt_output
 
-    Returns:
-        JSON:
-            {
-                "Rt": [
-                    {"date": <timestamp in s>, "value": <number>},
-                    ...
-                ],
-                "high90": [
-                    {"date": <timestamp in s>, "value": <number>},
-                    ...
-                ],
-                "low90": [
-                    {"date": <timestamp in s>, "value": <number>},
-                    ...
-                ],
-
-            }
+@cloudfunction(
+    in_schema=rt_input,
+    out_schema=rt_output,
+)
+def calculate_rt(request_json):
     """
-    request_json = request.get_json(silent=True)
+        accepts an object deserialized from JSON.
+        returns an object that can be serialized to JSON.
+        see schemas in @cloudfunction decorator for details.
+    """
     resp = defaultdict(list)
 
-    if request_json and 'dates' in request_json and 'cases' in request_json:
-        # TODO: real calculation
-        for date in request_json['dates']:
-            resp['Rt'].append({'date': date, 'value': 1.8})
-            resp['low90'].append({'date': date, 'value': 0.6})
-            resp['high90'].append({'date': date, 'value': 3.7})
+    # TODO: real calculation
+    for date in request_json['dates']:
+        resp['Rt'].append({'date': date, 'value': 1.8})
+        resp['low90'].append({'date': date, 'value': 0.6})
+        resp['high90'].append({'date': date, 'value': 3.7})
 
-    return json.dumps(resp)
+    return resp
--- backend/python-functions/test.py
+++ backend/python-functions/test.py
@@ -5,14 +5,25 @@ from unittest.mock import Mock
 from main import calculate_rt
 
 class TestCalculateRt(TestCase):
+    def setUp(self):
+        self.req = Mock(get_json=Mock())
+
+    def get_response_json(self):
+        (response_body, _, _) = calculate_rt(self.req)
+        return json.loads(response_body)
+
     def test_happy_path(self):
         data = {
         'dates': [1587422775, 1587509175, 1587595575, 1587681975],
         'cases': [5, 3, 0, 12]
         }
-        req = Mock(get_json=Mock(return_value=data))
+        self.req.get_json.return_value = data
 
-        resp = json.loads(calculate_rt(req))
+        resp = self.get_response_json()
+
+        # TODO: this is really just a placeholder test.
+        # is 1:1 correspondence between input and output dates
+        # actually what we want? probably not
         for date in data['dates']:
             rt = next(x for x in resp['Rt'] if x['date'] == date)
             self.assertIsInstance(rt['value'], float)
@@ -25,7 +36,8 @@ class TestCalculateRt(TestCase):
 
 
     def test_invalid_input(self):
-        req = Mock(get_json=Mock(return_value={}))
+        self.req.get_json.return_value = {}
 
-        resp = json.loads(calculate_rt(req))
+        resp = self.get_response_json()
+
         self.assertEqual(resp, {})
