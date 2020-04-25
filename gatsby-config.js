65ee7569db51a18d1dea72546080bf20089eee9f
Sean Zhu <interestinglythere@gmail.com>
2020-04-25 14:40:10 -0700

    Support favicon and web manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
 module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
@@ -5,5 +7,19 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
     "gatsby-plugin-typescript",
+
+    // prettier-ignore
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        short_name: "COVID-19",
+        name: "COVID-19 Dashboard",
+        icon: "src/site-metadata/icon-512.png",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    },
   ],
 };
