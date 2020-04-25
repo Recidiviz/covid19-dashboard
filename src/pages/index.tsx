b1029582bda9a11f2131586be839d0d3147692fd
Sean Zhu <interestinglythere@gmail.com>
2020-04-25 15:10:07 -0700

    Support private pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+import AuthWall from "../auth/AuthWall";
 import MultiFacilityPage from "../page-multi-facility/MultiFacilityPage";
 import PageInfo from "../site-metadata/PageInfo";
 
 // eslint-disable-next-line react/display-name
 export default () => (
-  <>
+  <AuthWall>
     <PageInfo title={undefined} />
     <MultiFacilityPage />
-  </>
+  </AuthWall>
 );
--- src/pages/inspect-model.tsx
+++ src/pages/inspect-model.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+import AuthWall from "../auth/AuthWall";
 import ModelInspectionPage from "../page-model-inspection/";
 import PageInfo from "../site-metadata/PageInfo";
 
 // eslint-disable-next-line react/display-name
 export default () => (
-  <>
+  <AuthWall>
     <PageInfo title="Inspect Epidemic Model Output" />
     <ModelInspectionPage />
-  </>
+  </AuthWall>
 );
