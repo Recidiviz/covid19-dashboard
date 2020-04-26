8e1acdaf74c5c6757d2d061e55e7476bed6471bc
Sean Zhu <interestinglythere@gmail.com>
2020-04-26 00:42:57 -0700

    Fix input form not saving in IE11

--- src/page-multi-facility/FacilityInputForm.tsx
+++ src/page-multi-facility/FacilityInputForm.tsx
@@ -7,6 +7,7 @@ import InputButton from "../design-system/InputButton";
 import InputDescription from "../design-system/InputDescription";
 import InputNameWithIcon from "../design-system/InputNameWithIcon";
 import ChartArea from "../impact-dashboard/ChartArea";
+import { PlannedRelease } from "../impact-dashboard/EpidemicModelContext";
 import ImpactProjectionTable from "../impact-dashboard/ImpactProjectionTableContainer";
 import MitigationInformation from "../impact-dashboard/MitigationInformation";
 import useModel from "../impact-dashboard/useModel";
@@ -43,12 +44,35 @@ const FacilityInputForm: React.FC = () => {
   const model = useModel();
 
   const save = () => {
+    // When saving an object to Firestore, Firestore will reject it if it is not
+    // serializable. This type enforces that what we are saving is serializable.
+    // In the future, we add new properties to the model that are not
+    // serializable, we will get a TS error below.
+    //
+    // Definition inspired by the one posted here:
+    // https://github.com/microsoft/TypeScript/issues/1897#issuecomment-338650717
+    type JsonSerializable =
+      | boolean
+      | number
+      | Date
+      | string
+      | undefined
+      | JsonSerializable[]
+      | { [key: string]: JsonSerializable };
+
+    let modelInputs: JsonSerializable = {
+      ...model[0],
+
+      // We don't need to save localeDataSource.
+      localeDataSource: undefined,
+    };
+
     saveFacility({
       id: facility?.id,
       name: facilityName || null,
       description: description || null,
       systemType: systemType || null,
-      modelInputs: JSON.parse(JSON.stringify(model))[0],
+      modelInputs,
     }).then((_) => {
       navigate("/");
     });
