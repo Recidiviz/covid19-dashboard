71f8bbab5e7bf34e6af1efd0f1abfe0a9d9bfeae
Jen <jovergaag@gmail.com>
2020-04-27 13:10:27 -0700

    add existing MitigationInformation with edits to RateOfSpread

--- src/page-multi-facility/RateOfSpreadSection.tsx
+++ src/page-multi-facility/RateOfSpreadSection.tsx
@@ -3,24 +3,7 @@ import React from "react";
 import styled from "styled-components";
 
 import Colors from "../design-system/Colors";
-import InputSelect from "../design-system/InputSelect";
-import Description from "../impact-dashboard/Description";
-import {
-  PlannedRelease,
-  PlannedReleases,
-  RateOfSpread,
-} from "../impact-dashboard/EpidemicModelContext";
-import {
-  FormGrid,
-  FormGridCell,
-  FormGridRow,
-} from "../impact-dashboard/FormGrid";
-import {
-  rateOfSpreadDisplayText,
-  ReleaseRow,
-  ReleaseUpdate,
-} from "../impact-dashboard/MitigationInformation";
-import useModel from "../impact-dashboard/useModel";
+import MitigationInformation from "../impact-dashboard/MitigationInformation";
 
 const borderStyle = `1px solid ${Colors.paleGreen}`;
 
@@ -39,69 +22,10 @@ const SectionHeader = styled.header`
 `;
 
 const RateOfSpreadSection: React.FC = () => {
-  const [
-    { plannedReleases = [{}], rateOfSpreadFactor },
-    updateModel,
-  ] = useModel();
-  // all updates should be happen on this mutable copy,
-  // which will replace the model state after user input
-  const mutableReleases = cloneDeep(plannedReleases);
-
-  const updateReleases = (newValue: PlannedReleases): void => {
-    updateModel({ plannedReleases: newValue });
-  };
-
-  const updateRelease = (opts: ReleaseUpdate): void => {
-    const { index, update } = opts;
-    mutableReleases[index] = Object.assign(
-      mutableReleases[index] || {},
-      update,
-    );
-    updateReleases(mutableReleases);
-  };
-
   return (
     <RateOfSpreadDiv>
       <SectionHeader>Rate of Spread</SectionHeader>
-      <Description className="mb-10">
-        Select the most likely rate of spread. This will change the R0 (rate of
-        spread) in the model.
-      </Description>
-      <FormGrid>
-        <FormGridRow>
-          <FormGridCell>
-            <InputSelect
-              onChange={(e) =>
-                updateModel({
-                  rateOfSpreadFactor: e.target.value as RateOfSpread,
-                })
-              }
-              value={rateOfSpreadFactor}
-            >
-              {Object.values(RateOfSpread).map((val) => (
-                <option key={val} value={val}>
-                  {rateOfSpreadDisplayText[val]}
-                </option>
-              ))}
-            </InputSelect>
-          </FormGridCell>
-        </FormGridRow>
-      </FormGrid>
-      <Description>
-        Enter planned reductions in the in-facility population (e.g., early
-        releases to supervision).
-      </Description>
-      <FormGrid>
-        {mutableReleases?.map(({ date, count }, index) => (
-          <ReleaseRow
-            key={index}
-            date={date}
-            count={count}
-            index={index}
-            updateRelease={updateRelease}
-          />
-        ))}
-      </FormGrid>
+      <MitigationInformation />
     </RateOfSpreadDiv>
   );
 };
