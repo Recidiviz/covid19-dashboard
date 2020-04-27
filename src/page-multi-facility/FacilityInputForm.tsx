b725e1289a0444cc554cdb371d917952e3ad7363
rocktacular <captain.rocktacular@gmail.com>
2020-04-27 11:45:44 -0500

    Coiln/208/delete-button (#224)
    * create popup menu component
    
    * move delete facility logic from home to /facility
    
    * cleanup structure
    
    * shrink popup menu container height
    
    * move PopUpMenu to description row
    
    * style menu to design
    
    * add box shadow
    
    * update deleteFacility with scenarioId
    
    Co-authored-by: Colin Folta <colin.folta@gmail.com>

--- src/page-multi-facility/FacilityInputForm.tsx
+++ src/page-multi-facility/FacilityInputForm.tsx
@@ -2,10 +2,13 @@ import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
-import { saveFacility } from "../database/index";
-import InputButton from "../design-system/InputButton";
+import { deleteFacility, saveFacility } from "../database/index";
+import Colors from "../design-system/Colors";
+import InputButton, { StyledButton } from "../design-system/InputButton";
 import InputDescription from "../design-system/InputDescription";
 import InputNameWithIcon from "../design-system/InputNameWithIcon";
+import ModalDialog from "../design-system/ModalDialog";
+import PopUpMenu from "../design-system/PopUpMenu";
 import ChartArea from "../impact-dashboard/ChartArea";
 import ImpactProjectionTable from "../impact-dashboard/ImpactProjectionTableContainer";
 import MitigationInformation from "../impact-dashboard/MitigationInformation";
@@ -29,6 +32,51 @@ const RightColumn = styled.div`
 
 const ButtonSection = styled.div`
   margin-top: 30px;
+`;
+
+const DescRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PopUpMenuWrapper = styled.div`
+  padding-top: 12px;
+`;
+
+// Delete Modal elements
+const ModalContents = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  font-weight: normal;
+  justify-content: flex-start;
+  margin-top: 30px;
+`;
+
+const ModalText = styled.div`
+  font-size: 13px;
+  margin-right: 25px;
+`;
+
+const ModalButtons = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+`;
+
+const ModalButton = styled(StyledButton)`
+  font-size: 14px;
+  font-weight: normal;
+`;
+const DeleteButton = styled(ModalButton)`
+  background: ${Colors.darkRed};
+  color: ${Colors.white};
+  margin-right: 15px;
+`;
+
+const CancelButton = styled(ModalButton)`
+  background: transparent;
+  border: 1px solid ${Colors.forest};
+  color: ${Colors.forest};
 `;
 
 interface Props {
@@ -59,6 +107,24 @@ const FacilityInputForm: React.FC<Props> = ({ scenarioId }) => {
     });
   };
 
+  // delete modal stuff
+  const [showDeleteModal, updateShowDeleteModal] = useState(false);
+  const openDeleteModal = () => {
+    updateShowDeleteModal(true);
+  };
+  const closeDeleteModal = () => {
+    updateShowDeleteModal(false);
+  };
+  const popupItems = [{ name: "Delete", onClick: openDeleteModal }];
+  const removeFacility = async () => {
+    const facilityId = facility?.id;
+    if (facilityId) {
+      await deleteFacility(scenarioId, facilityId);
+      history.goBack();
+    }
+    updateShowDeleteModal(false);
+  };
+
   return (
     <FacilityInputFormDiv>
       <LeftColumn>
@@ -67,11 +133,18 @@ const FacilityInputForm: React.FC<Props> = ({ scenarioId }) => {
           setName={setFacilityName}
           placeholderValue="Unnamed Facility"
         />
-        <InputDescription
-          description={description}
-          setDescription={setDescription}
-          placeholderValue="Enter a description (optional)"
-        />
+        <DescRow>
+          <InputDescription
+            description={description}
+            setDescription={setDescription}
+            placeholderValue="Enter a description (optional)"
+          />
+          <PopUpMenuWrapper>
+            <PopUpMenu items={popupItems} />
+          </PopUpMenuWrapper>
+        </DescRow>
+        <div className="mt-5 mb-5 border-b border-gray-300" />
+
         <LocaleInformationSection
           systemType={systemType}
           setSystemType={setSystemType}
@@ -87,6 +160,26 @@ const FacilityInputForm: React.FC<Props> = ({ scenarioId }) => {
         <ChartArea />
         <ImpactProjectionTable />
       </RightColumn>
+
+      {/* MODAL */}
+      <ModalDialog
+        closeModal={closeDeleteModal}
+        open={showDeleteModal}
+        title="Are you sure?"
+      >
+        <ModalContents>
+          <ModalText>This action cannot be undone.</ModalText>
+          <ModalButtons>
+            <DeleteButton
+              label="Delete facility"
+              onClick={removeFacility} // replace with actual delete function (pass ID)
+            >
+              Delete facility
+            </DeleteButton>
+            <CancelButton onClick={closeDeleteModal}>Cancel</CancelButton>
+          </ModalButtons>
+        </ModalContents>
+      </ModalDialog>
     </FacilityInputFormDiv>
   );
 };
