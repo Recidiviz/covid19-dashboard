1d350fec51309db3e7f25cbb0fd75c40f78e240e
Daniela Schiano di Cola <daschi87@gmail.com>
2020-04-27 21:37:20 -0700

    Add PageLayoutColumns styled elements

---
+++ src/design-system/PageLayoutColumns.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const LeftColumn = styled.div`
+  margin: 20px;
+  width: 45%;
+`;
+export const RightColumn = styled.div`
+  margin: 20px;
+  width: 55%;
+`;
