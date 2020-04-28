00ea8e37f41b6706b14adaa4ae2a96f013c5cabc
Amy Lam <amy.r.lam@gmail.com>
2020-04-28 16:46:42 -0700

    Use Colors variable to be DRY (#248)
    Follow-on to https://github.com/Recidiviz/covid19-dashboard/pull/241

--- src/design-system/TextLabel.tsx
+++ src/design-system/TextLabel.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
+import Colors from "./Colors";
+
 const TextLabel = styled.span<{ softened?: boolean }>`
   ${(props) => (props.softened ? null : "text-transform: uppercase;")}
   font-size: 12px;
   font-weight: 100;
   font-family: "Poppins", sans-serif;
   ${(props) => (props.softened ? null : "letter-spacing: 2px;")}
-  color: #033342;
+  color: ${Colors.darkForest};
   padding-right: 5px;
 `;
 
