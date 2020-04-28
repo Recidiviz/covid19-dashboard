0db427d1a5dc3dbf47c2a18f18898d5ddaffca51
Amy Lam <amy.r.lam@gmail.com>
2020-04-28 14:36:54 -0700

    Change styling of "Facility Details" input labels
    With the font styling change of the inputs, the input labels such as "FACILITY STAFF", "CURRENT CASES" didn't match what's in Figma.
    
    Ref: https://www.figma.com/file/EGJS9njuKHWdFHUS4lrHA4/%F0%9F%9A%80Recidiviz-CJStatus?node-id=910%3A130

--- src/design-system/TextLabel.tsx
+++ src/design-system/TextLabel.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
+import Colors from "./Colors";
+
 const TextLabel = styled.span<{ softened?: boolean }>`
   ${(props) => (props.softened ? null : "text-transform: uppercase;")}
-  font-size: 12px;
-  font-weight: 100;
+  font-size: 10px;
+  font-weight: 400;
   font-family: "Poppins", sans-serif;
   ${(props) => (props.softened ? null : "letter-spacing: 2px;")}
-  color: #033342;
+  color: ${Colors.darkForest};
   padding-right: 5px;
 `;
 
