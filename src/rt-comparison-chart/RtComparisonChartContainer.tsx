e72020e65e9c89ea078314289443b2a6408b781c
Ian MacFarland <ian@recidiviz.org>
2020-05-07 15:30:56 -0700

    clean up fake data

--- src/rt-comparison-chart/RtComparisonChartContainer.tsx
+++ src/rt-comparison-chart/RtComparisonChartContainer.tsx
@@ -5,23 +5,22 @@ import RtComparisonChart from "./RtComparisonChart";
 const RtComparisonChartContainer: React.FC = () => {
   // TODO: not this
   const fakeData = [
-    {
-      name: "facility 1",
-      id: "abc123",
-      values: { Rt: 1.9, low90: 0.4, high90: 3.8 },
-    },
-    {
-      name:
-        "facility 2 what has a long name that may not fit on my god how long does it have to be",
-      id: "def456",
-      values: {
-        Rt: 0.6,
-        low90: 0,
-        high90: 1.5,
-      },
-    },
-    { name: "facility with no data and a long name", id: "xyz789", values: {} },
-  ];
+    // {
+    //   name: "facility 1",
+    //   id: "abc123",
+    //   values: { Rt: 1.9, low90: 0.4, high90: 3.8 },
+    // },
+    // {
+    //   name: "facility 2 what has a long name ",
+    //   id: "def456",
+    //   values: {
+    //     Rt: 0.6,
+    //     low90: 0,
+    //     high90: 1.5,
+    //   },
+    // },
+    // { name: "facility with no data and a long name", id: "xyz789", values: {} },
+  ] as any;
 
   return <RtComparisonChart data={fakeData} />;
 };
