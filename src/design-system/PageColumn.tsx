1fb0cf7b40004cf43ab84b38f8f5b3ac0e69d4ea
Amy Lam <amy.r.lam@gmail.com>
2020-05-11 13:34:17 -0700

    Make /impact layout more responsive

--- src/design-system/PageColumn.tsx
+++ src/design-system/PageColumn.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: row;
+
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
 `;
 export const Column = styled.div<{ width?: string }>`
   margin: 20px;
