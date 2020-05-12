d4ef967c1458f67be799f05fac7bfd92e7fd4b22
Amy Lam <amy.r.lam@gmail.com>
2020-05-12 10:59:35 -0700

    Remove unneeded space

--- src/page-response-impact/styles.tsx
+++ src/page-response-impact/styles.tsx
@@ -83,7 +83,7 @@ export const ReportDateDiv = styled.div`
 `;
 
 export const DescriptionTextDiv = styled.div`
-  font-family:  "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 13px;
