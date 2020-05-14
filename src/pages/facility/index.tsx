5d409e1137a4ee0ad69fdaaa6f2623c38a9ddaac
Leonard Bogdonoff <rememberlenny@gmail.com>
2020-05-14 14:26:14 -0400

    Save prop

--- src/pages/facility/index.tsx
+++ src/pages/facility/index.tsx
@@ -5,11 +5,14 @@ import FacilityPage from "../../page-multi-facility/FacilityPage";
 import PageInfo from "../../site-metadata/PageInfo";
 
 // eslint-disable-next-line react/display-name
-export default () => (
-  <>
-    <PageInfo title="Facility" />
-    <AuthWall>
-      <FacilityPage />
-    </AuthWall>
-  </>
-);
+export default (props: any) => {
+  console.log("props", props);
+  return (
+    <>
+      <PageInfo title="Facility" />
+      <AuthWall>
+        <FacilityPage />
+      </AuthWall>
+    </>
+  );
+};
