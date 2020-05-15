946f34499a9ac101d6e8eea320247f44cada57c7
Ian MacFarland <ian@recidiviz.org>
2020-05-15 13:05:18 -0700

    add feature flag for sharing UI

--- src/feature-flags/featureFlags.tsx
+++ src/feature-flags/featureFlags.tsx
@@ -5,6 +5,8 @@ export type FeatureFlags = {
   // TODO: Delete this flag once #202 is done
   showRateOfSpreadTab: boolean;
   showImpactButton: boolean;
+  // TODO: can remove after #169 is successfully released to prod
+  enableSharing: boolean;
 };
 
 const { FlagsProvider, Flag, useFlag, useFlags } = createFlags<FeatureFlags>();
@@ -15,6 +17,7 @@ export const FeatureFlagsProvider: React.FC = ({ children }) => {
       flags={{
         showRateOfSpreadTab: true,
         showImpactButton: process.env.GATSBY_SHOW_IMPACT_BUTTON === "true",
+        enableSharing: process.env.GATSBY_ENABLE_SHARING === "true",
       }}
     >
       {children}
