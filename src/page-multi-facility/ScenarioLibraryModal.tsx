597a0b049f37e3453265b0bece4a993f597bbbf3
Sokunthel Chheang <sokunthel@gmail.com>
2020-06-09 14:34:41 -0700

    #524 fix overlap issue in Scenario Library Cards (#545)
    * #524 fix overlap issue in Scenario Library Cards
    
    * #524 keep pulsing/loading center vertically & horizontally

--- src/page-multi-facility/ScenarioLibraryModal.tsx
+++ src/page-multi-facility/ScenarioLibraryModal.tsx
@@ -38,7 +38,7 @@ const ScenarioLibrary = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  height: 100%;
+  height: auto;
 `;
 
 const ScenarioCard = styled.div`
@@ -277,11 +277,11 @@ const ScenarioLibraryModal: React.FC<Props> = ({ trigger }) => {
 
   const ScenarioLibraryWrapper = (props: any) => {
     return (
-      <ScenarioLibrary>
+      <>
         {props.scenarios?.loading ? (
           <Loading />
         ) : (
-          <>
+          <ScenarioLibrary>
             {props.ownedFlag && <CreateNewScenarioModal />}
             {props.scenarios?.data.map((scenario: any) => {
               const popupItems = [
@@ -326,9 +326,9 @@ const ScenarioLibraryModal: React.FC<Props> = ({ trigger }) => {
                 </ScenarioCard>
               );
             })}
-          </>
+          </ScenarioLibrary>
         )}
-      </ScenarioLibrary>
+      </>
     );
   };
 
