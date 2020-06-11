36fb90b4deb903172b86e07eccf5789761cbf344
Daniela Schiano di Cola <daschi87@gmail.com>
2020-06-10 22:39:15 -0700

    Set internal state when state is selected

--- src/page-weekly-snapshot/WeeklySnapshotPage.tsx
+++ src/page-weekly-snapshot/WeeklySnapshotPage.tsx
@@ -1,13 +1,22 @@
-import { maxBy, minBy } from "lodash";
+import { maxBy, minBy, orderBy } from "lodash";
 import numeral from "numeral";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import { DateMMMMdyyyy } from "../design-system/DateFormats";
 import InputSelect from "../design-system/InputSelect";
 import Loading from "../design-system/Loading";
-import { LocaleDataProvider, useLocaleDataState } from "../locale-data-context";
-import { NYTData, NYTStateRecord, useNYTData } from "./NYTDataProvider";
+import {
+  LocaleDataProvider,
+  LocaleRecord,
+  useLocaleDataState,
+} from "../locale-data-context";
+import {
+  NYTCountyRecord,
+  NYTData,
+  NYTStateRecord,
+  useNYTData,
+} from "./NYTDataProvider";
 
 const stateNamesFilter = (key: string) =>
   !["US Total", "US Federal Prisons"].includes(key);
@@ -18,13 +27,82 @@ const LocaleStats = styled.div`
 `;
 const LocaleStatsList = styled.ul``;
 
+type PerCapitaCountyCase = {
+  name: string;
+  perCapitaIncrease: number | undefined;
+};
+
+function getDay(nytData: NYTCountyRecord[] | NYTStateRecord[], day: number) {
+  if (day === 1) {
+    return minBy(nytData, (d: NYTCountyRecord | NYTStateRecord) => d.date);
+  } else {
+    return maxBy(nytData, (d: NYTCountyRecord | NYTStateRecord) => d.date);
+  }
+}
+
+function getCountyDataByName(counties: NYTCountyRecord[]) {
+  const dataByCounty: { [countyName: string]: NYTCountyRecord[] } = {};
+
+  counties.forEach((data: NYTCountyRecord) => {
+    if (dataByCounty[data.county]) {
+      dataByCounty[data.county] = [...dataByCounty[data.county], data];
+    } else {
+      dataByCounty[data.county] = [data];
+    }
+  });
+
+  return dataByCounty;
+}
+
+function calculatePerCapitaIncrease(
+  daySevenCases: number | undefined,
+  dayOneCases: number | undefined,
+  countyPopulation: number | undefined,
+) {
+  if (
+    (!daySevenCases && daySevenCases !== 0) ||
+    (!dayOneCases && dayOneCases !== 0) ||
+    !countyPopulation
+  )
+    return;
+  return (daySevenCases - dayOneCases) / countyPopulation;
+}
+
+function getPerCapitaCountyCases(
+  counties: NYTCountyRecord[],
+  stateLocaleData: Map<string, LocaleRecord> | undefined,
+) {
+  const dataByCounty = getCountyDataByName(counties);
+  const perCapitaCountyCases: PerCapitaCountyCase[] = [];
+
+  for (const countyName in dataByCounty) {
+    const dayOne = getDay(dataByCounty[countyName], 1);
+    const daySeven = getDay(dataByCounty[countyName], 7);
+    const countyPopulation = stateLocaleData?.get(countyName)?.totalPopulation;
+    perCapitaCountyCases.push({
+      name: countyName,
+      perCapitaIncrease: calculatePerCapitaIncrease(
+        daySeven?.cases,
+        dayOne?.cases,
+        countyPopulation,
+      ),
+    });
+  }
+  return perCapitaCountyCases;
+}
+
 export default function WeeklySnapshotPage() {
   const { data, loading: nytLoading } = useNYTData();
-  console.log({ data });
   const [selectedState, setSelectedState] = useState<NYTData | undefined>();
+  const [sevenDayDiffInCases, setSevenDayDiffInCases] = useState<
+    number | undefined
+  >();
+  const [countiesToWatch, setCountiesToWatch] = useState<string[] | undefined>(
+    [],
+  );
+  const [totalBeds, setTotalBeds] = useState<number | undefined>();
   const { data: localeData, loading } = useLocaleDataState();
   const stateNames = Array.from(localeData.keys()).filter(stateNamesFilter);
-
   const handleOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const stateName = event.target.value;
     if (!nytLoading) {
@@ -32,17 +110,38 @@ export default function WeeklySnapshotPage() {
     }
   };
 
-  const dayOne =
-    selectedState &&
-    minBy(selectedState.state, (state: NYTStateRecord) => state.date);
-  const daySeven =
-    selectedState &&
-    maxBy(selectedState.state, (state: NYTStateRecord) => state.date);
-  const sevenDayDiffInCases =
-    daySeven && dayOne && daySeven.cases - dayOne.cases;
-  const total =
-    dayOne && dayOne.state && localeData?.get(dayOne.state)?.get("Total");
-  const totalBeds = total && total.icuBeds + total.hospitalBeds;
+  const dayOne = getDay(selectedState?.state || [], 1);
+  const daySeven = getDay(selectedState?.state || [], 7);
+  useEffect(() => {
+    if (dayOne?.state && daySeven?.state && selectedState) {
+      const stateLocaleData = localeData?.get(dayOne.state);
+      const totalLocaleData = stateLocaleData?.get("Total");
+      const sevenDayDiffInCases = daySeven.cases - dayOne.cases;
+      const totalBeds =
+        totalLocaleData &&
+        totalLocaleData.icuBeds + totalLocaleData.hospitalBeds;
+
+      const perCapitaCountyCases = getPerCapitaCountyCases(
+        selectedState.counties,
+        stateLocaleData,
+      );
+      const highestFourCounties = orderBy(
+        perCapitaCountyCases.filter((c) => !!c.perCapitaIncrease),
+        ["perCapitaIncrease"],
+        ["desc"],
+      ).slice(0, 4);
+      const countiesToWatch = highestFourCounties.map((county) => {
+        const starred =
+          stateLocaleData && stateLocaleData.has(county.name) ? "***" : "";
+        return `${county.name}${starred}, ${numeral(
+          county.perCapitaIncrease,
+        ).format("0.000 %")};`;
+      });
+      setCountiesToWatch(countiesToWatch);
+      setTotalBeds(totalBeds);
+      setSevenDayDiffInCases(sevenDayDiffInCases);
+    }
+  }, [selectedState, localeData]);
 
   return (
     <LocaleDataProvider>
@@ -67,11 +166,13 @@ export default function WeeklySnapshotPage() {
               <LocaleStatsList>
                 <li>
                   Latest Date:{" "}
-                  {daySeven && <DateMMMMdyyyy date={daySeven.date} />}
+                  {daySeven?.date && <DateMMMMdyyyy date={daySeven.date} />}
                 </li>
                 <li>
                   Number of cases in the state:{" "}
-                  {daySeven ? formatNumber(daySeven.cases) : "?"}
+                  {daySeven?.cases || daySeven?.cases === 0
+                    ? formatNumber(daySeven.cases)
+                    : "?"}
                 </li>
                 <li>
                   Change in state cases since last week:{" "}
@@ -83,7 +184,7 @@ export default function WeeklySnapshotPage() {
                   Number of ICU and hospital beds in state:{" "}
                   {totalBeds || totalBeds === 0 ? formatNumber(totalBeds) : "?"}
                 </li>
-                <li>Counties to watch: </li>
+                <li>Counties to watch: {countiesToWatch?.join(" ")}</li>
               </LocaleStatsList>
             </LocaleStats>
           )}
