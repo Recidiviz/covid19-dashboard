46f743b2943c78ef5256298ba0910a1571334a70
Jen <jovergaag@gmail.com>
2020-06-16 14:23:42 -0700

    add FacilityProjections placeholders to WeeklySnapshotPage

--- src/page-weekly-snapshot/WeeklySnapshotPage.tsx
+++ src/page-weekly-snapshot/WeeklySnapshotPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import Colors from "../design-system/Colors";
+import FacilityProjections from "./FacilityProjections";
 import LocaleSummary from "./LocaleSummary";
 
 const WeeklySnapshotContainer = styled.div``;
@@ -8,12 +10,13 @@ const WeeklySnapshotContainer = styled.div``;
 const Placeholder = styled.div`
   padding: 20px;
   margin: 20px;
+  font-size: 18px;
 `;
 
 export default function WeeklySnapshotPage() {
   return (
     <WeeklySnapshotContainer>
-      <div className="font-body text-black min-h-screen tracking-normal w-full">
+      <div className="font-body min-h-screen tracking-normal w-full">
         <div className="max-w-screen-xl px-4 mx-auto">
           <Placeholder>Page Header</Placeholder>
           <Placeholder>Year To Date Summary and Impact Report</Placeholder>
@@ -21,7 +24,10 @@ export default function WeeklySnapshotPage() {
             System Summary
             <LocaleSummary />
           </Placeholder>
-          <Placeholder>Facility Pages</Placeholder>
+          <Placeholder>
+            Facility Projections
+            <FacilityProjections />
+          </Placeholder>
           <Placeholder>Page Footer</Placeholder>
         </div>
       </div>
