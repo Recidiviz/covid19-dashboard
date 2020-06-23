45efa4478a78a58255e915e87f9585111772d8d8
Neehar Banerjee <neehar@stanford.edu>
2020-06-23 17:18:48 -0500

    (not working) new component for facility charts

---
+++ src/page-multi-facility/FacilityChart.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+
+import { getFacilities } from "../database";
+import Loading from "../design-system/Loading";
+import useRejectionToast from "../hooks/useRejectionToast";
+import { EpidemicModelProvider } from "../impact-dashboard/EpidemicModelContext";
+import { getRtDataForFacility } from "../infection-model/rt";
+import { useLocaleDataState } from "../locale-data-context";
+import FacilityRow from "./FacilityRow";
+import FacilityRowPlaceholder from "./FacilityRowPlaceholder";
+import { Facility, RtValue, Scenario } from "./types";
+
+const FacilityChart: React.FC<{
+  scenarioId: string;
+}> = ({ scenarioId }) => {
+  const [firstFacility, setFirstFacility] = useState<Facility | null>();
+  const [
+    firstFacilityRtData,
+    setFirstFacilityRtData,
+  ] = useState<RtValue | null>();
+  const { data: localeDataSource } = useLocaleDataState();
+
+  const handleFacilitySave = (facility: Facility) => {
+    console.log(facility);
+  };
+
+  const fetchFacility = async () => {
+    const facilities = await getFacilities(scenarioId);
+    const firstFacility = facilities?.[0];
+    if (firstFacility) {
+      const firstFacilityRtData = await getRtDataForFacility(firstFacility);
+      setFirstFacility(firstFacility);
+      setFirstFacilityRtData(firstFacilityRtData);
+    }
+  };
+
+  useEffect(() => {
+    fetchFacility();
+  }, [fetchFacility]);
+
+  return (
+    <>
+      {firstFacility && firstFacilityRtData ? (
+        <FacilityRowPlaceholder key={firstFacility?.id}>
+          <EpidemicModelProvider
+            facilityModel={firstFacility?.modelInputs}
+            localeDataSource={localeDataSource}
+          >
+            <FacilityRow
+              facility={firstFacility}
+              facilityRtData={firstFacilityRtData}
+              onSave={handleFacilitySave}
+            />
+          </EpidemicModelProvider>
+        </FacilityRowPlaceholder>
+      ) : (
+        <Loading />
+      )}
+      ;
+    </>
+  );
+};
+
+export default FacilityChart;
