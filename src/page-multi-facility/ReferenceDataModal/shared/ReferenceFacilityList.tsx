529e6d2779784a1e4c2360eaaf1ddbe6f4f27e36
Daniela Schiano di Cola <daschi87@gmail.com>
2020-06-25 12:07:07 -0700

    Only list reference facilities that are not already synced in the facility input form

--- src/page-multi-facility/ReferenceDataModal/shared/ReferenceFacilityList.tsx
+++ src/page-multi-facility/ReferenceDataModal/shared/ReferenceFacilityList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
+import { referenceFacilitiesProp } from "../../../database";
 import { useFacilities } from "../../../facilities-context";
+import useScenario from "../../../scenario-context/useScenario";
 import { ReferenceFacility } from "../../types";
 import ReferenceFacilityRow from "./ReferenceFacilityRow";
 
@@ -13,12 +15,23 @@ export const ReferenceFacilityList: React.FC<Props> = ({
   onClick,
   selectedId,
 }) => {
+  const [scenarioState] = useScenario();
+  const mappedReferenceFacilities =
+    scenarioState.data?.[referenceFacilitiesProp] || {};
+
   const {
     state: { referenceFacilities },
   } = useFacilities();
+
+  const facilities = Object.values(referenceFacilities).filter(
+    (refFacility) => {
+      return !Object.values(mappedReferenceFacilities).includes(refFacility.id);
+    },
+  );
+
   return (
     <>
-      {Object.values(referenceFacilities).map((refFacility) => (
+      {facilities.map((refFacility) => (
         <ReferenceFacilityRow
           key={refFacility.id}
           selected={selectedId === refFacility.id}
