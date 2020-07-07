86cad8bcb7fb41e16ac04a06d5710181682a291d
Jen <jovergaag@gmail.com>
2020-07-07 15:10:16 -0700

    remote WIP SystemWideProjectionChart work

--- src/page-weekly-snapshot/SystemWideProjectionChart.tsx
+++ src/page-weekly-snapshot/SystemWideProjectionChart.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-import Colors, { MarkColors } from "../design-system/Colors";
-import CurveChart from "../impact-dashboard/CurveChart";
+import Colors from "../design-system/Colors";
 import { LocaleData } from "../locale-data-context";
 import { Facilities } from "../page-multi-facility/types";
-import {
-  useCurrentCurveData,
-  useEpidemicModelState,
-  useSystemWideData,
-} from "../page-response-impact/hooks";
-import { getCurveChartData } from "../page-response-impact/responseChartData";
 
 const SystemWideProjectionChartContainer = styled.div`
   font-size: 12px;
   font-weight: 400;
+  padding: 20px 0;
 `;
 
 const CurveChartContainer = styled.div`
@@ -22,8 +16,6 @@ const CurveChartContainer = styled.div`
 `;
 
 export const ChartHeader = styled.h3`
-  border-top: 1px solid ${Colors.opacityGray};
-  border-bottom: 1px solid ${Colors.opacityGray};
   color: ${Colors.black};
   font-family: "Libre Franklin";
   font-style: normal;
@@ -41,28 +33,6 @@ const SystemWideProjectionChart: React.FC<Props> = ({
   localeData,
   facilities,
 }) => {
-  const epidemicModelState = useEpidemicModelState(facilities, localeData);
-  // const systemWideData = useSystemWideData(
-  //   scenario.data.baselinePopulations,
-  //   facilities,
-  //   epidemicModelState,
-  //   localeData,
-  // );
-  const currentCurveInputs = useCurrentCurveData(
-    epidemicModelState,
-    localeData,
-  );
-  // const currentCurveData = getCurveChartData(currentCurveInputs);
-
-  // <CurveChart
-  //   chartHeight={250}
-  //   hideAxes={false}
-  //   yAxisExtent={[0, 100]}
-  //   hospitalBeds={50}
-  //   markColors={MarkColors}
-  //   curveData={currentCurveData}
-  // />
-
   return (
     <SystemWideProjectionChartContainer>
       <CurveChartContainer>
