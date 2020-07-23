a698c85f08f20ecb848824d64ff4d0f3411862bd
Neehar Banerjee <neehar@stanford.edu>
2020-07-23 16:26:16 -0500

    (working) dates and formatting

--- src/page-weekly-snapshot/WeeklySnapshotPage.tsx
+++ src/page-weekly-snapshot/WeeklySnapshotPage.tsx
@@ -1,10 +1,16 @@
-import { format, startOfToday } from "date-fns";
+import { format, max, startOfToday, startOfYesterday } from "date-fns";
 import { get } from "lodash";
 import React from "react";
 import styled from "styled-components";
 
 import Loading from "../design-system/Loading";
+import { useFacilities } from "../facilities-context";
+import {
+  findMatchingDay,
+  findMostRecentDate,
+} from "../hooks/useAddCasesInputs";
 import { useLocaleDataState } from "../locale-data-context";
+import { Facility } from "../page-multi-facility/types";
 import FacilitySummaries from "./FacilitySummaries";
 import ImpactProjectionChart from "./ImpactProjectionChart";
 import LocaleStatsTable from "./LocaleStatsTable";
@@ -16,7 +22,6 @@ import {
   Right,
   STATE_CODE_MAPPING,
   TextContainer,
-  TextContainerHeading,
 } from "./shared";
 import SnapshotPage from "./SnapshotPage";
 import SystemWideProjectionChart from "./SystemWideProjectionChart";
@@ -25,13 +30,50 @@ import { useWeeklyReport } from "./weekly-report-context/WeeklyReportContext";
 const WeeklySnapshotPageDiv = styled.div``;
 const WeeklySnapshotContainer = styled.div``;
 
+function getMostRecentDOCRDate(facilities: Facility[]) {
+  let mostRecentDate = undefined;
+  for (let i = 0; i < facilities.length; i++) {
+    const facility = facilities[i];
+    const hasEarlierData = facility.modelVersions.length > 1;
+    let facilityMostRecentDate = undefined;
+
+    if (hasEarlierData) {
+      const currentDate = facility.updatedAt;
+      facilityMostRecentDate = findMostRecentDate(
+        currentDate,
+        facility.modelVersions,
+        false,
+      );
+      if (mostRecentDate === undefined) {
+        mostRecentDate = facilityMostRecentDate;
+      } else {
+        mostRecentDate = max([mostRecentDate, facilityMostRecentDate]);
+      }
+    }
+  }
+  if (mostRecentDate) {
+    return mostRecentDate;
+  }
+  return startOfToday();
+}
+
 const WeeklySnapshotPage: React.FC = () => {
   const localeState = useLocaleDataState();
   const {
     state: { stateName },
   } = useWeeklyReport();
+  const { state: facilitiesState } = useFacilities();
+  const facilities = Object.values(facilitiesState.facilities);
+
   const today = startOfToday();
   const todayFormatted = format(today, "LLLL dd, yyyy");
+
+  const mostRecentDOCRDate = getMostRecentDOCRDate(facilities);
+  const mostRecentDOCRDateFormatted = format(mostRecentDOCRDate, "MM/dd/yyyy");
+
+  const yesterday = startOfYesterday();
+  const yesterdayFormatted = format(yesterday, "MM/dd/yyyy");
+
   let stateImage = undefined;
   if (stateName && get(STATE_CODE_MAPPING, stateName)) {
     const stateCode = get(STATE_CODE_MAPPING, stateName);
@@ -60,7 +102,8 @@ const WeeklySnapshotPage: React.FC = () => {
                     impact on the incarcerated and staff
                   </LeftHeading>
                   <Right>
-                    DOCR data as of: xx/xx/xx Community cases as of: xx/xx/xx
+                    DOCR data as of: {mostRecentDOCRDateFormatted} Community
+                    cases as of: {yesterdayFormatted}
                   </Right>
                 </TextContainer>
                 <HorizontalRule />
