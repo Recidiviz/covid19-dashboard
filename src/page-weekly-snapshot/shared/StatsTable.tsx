6dfbd757d85ead2468f0e76282c16b704f283e60
Daniela Schiano di Cola <daschi87@gmail.com>
2020-07-29 16:33:24 -0700

    Add a separate StatsTableRow component

--- src/page-weekly-snapshot/shared/StatsTable.tsx
+++ src/page-weekly-snapshot/shared/StatsTable.tsx
@@ -10,15 +10,56 @@ import {
   Value,
   TextContainer,
   SectionHeader,
+  SectionSubheader,
 } from ".";
+
+type ColumnData = {
+  header: string;
+  value: string;
+  valueDescription?: () => React.ReactElement | undefined
+}
+
+interface StatsTableRowProps {
+  columns: ColumnData[];
+  columnMarginRight?: string;
+}
+
+export const StatsTableRow: React.FC<StatsTableRowProps> = ({
+  columns,
+  columnMarginRight = "0px"
+}) => {
+  return (
+    <tr>
+      {
+        columns.map((columnData: ColumnData) => (
+          <TableCell>
+            <TableCellContainer marginRight={columnMarginRight}>
+              <BorderDiv />
+              <Header>{columnData.header}</Header>
+              <HorizontalRule />
+              <TextContainer>
+                <Value>{columnData.value}</Value>
+                {columnData.valueDescription && columnData.valueDescription()}
+              </TextContainer>
+            </TableCellContainer>
+          </TableCell>
+        ))
+      }
+    </tr>
+  )
+}
 
 interface StatesTableProps {
   tableHeading: string;
-  tableData: any;
-  columnMarginRight: string;
+  tableSubheading?: string;
+  children: React.ReactNode;
 }
 
-const StatsTable: React.FC<StatesTableProps> = ({ tableHeading, tableData, columnMarginRight = "0px" }) => {
+const StatsTable: React.FC<StatesTableProps> = ({
+  tableHeading,
+  tableSubheading,
+  children,
+}) => {
   return (
     <>
       <HorizontalRule />
@@ -28,28 +69,13 @@ const StatsTable: React.FC<StatesTableProps> = ({ tableHeading, tableData, colum
             <TableHeading>
               <TextContainer>
                 <SectionHeader>{tableHeading}</SectionHeader>
+                {tableSubheading && <SectionSubheader>{tableSubheading}</SectionSubheader>}
               </TextContainer>
             </TableHeading>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {
-              tableData.map((columnData: any) => (
-                <TableCell>
-                  <TableCellContainer marginRight={columnMarginRight}>
-                    <BorderDiv />
-                    <Header>{columnData.header}</Header>
-                    <HorizontalRule />
-                    <TextContainer>
-                      <Value>{columnData.value}</Value>
-                      {columnData.valueDescription}
-                    </TextContainer>
-                  </TableCellContainer>
-                </TableCell>
-              ))
-            }
-          </tr>
+          {children}
         </tbody>
       </Table>
       <HorizontalRule />
