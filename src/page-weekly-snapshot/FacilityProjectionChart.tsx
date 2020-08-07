cf5e89dd7da41db85e99756278d5794a1c10c7bb
Neehar Banerjee <29155017+neeharmb@users.noreply.github.com>
2020-08-07 15:44:33 -0500

    [Report Automation] Projection Bugs (#676)
    * (working) y-axis height
    
    * (working) switch colors between infectious/hospitalized
    
    * (working) tick values for x axis
    
    * (working) only calculate x tick values once
    
    * PR comments

--- src/page-weekly-snapshot/FacilityProjectionChart.tsx
+++ src/page-weekly-snapshot/FacilityProjectionChart.tsx
@@ -1,5 +1,5 @@
 import { curveCatmullRom, format } from "d3";
-import { add, format as formatDate } from "date-fns";
+import { add, format as formatDate, startOfToday } from "date-fns";
 import { flatten, pick } from "lodash";
 import React from "react";
 import { ResponsiveXYFrame } from "semiotic";
@@ -44,10 +44,27 @@ export interface ChartData {
 const formatThousands = format(",~g");
 const formatRtValue = format(".2f");
 
+const PROJECTION_DURATION = 90;
+const numXTicks = 4;
+const tickIntervals = PROJECTION_DURATION / numXTicks;
+const xTickValues: Date[] = [];
+
+function addDays(date: Date, days: number) {
+  let result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+let increment = 0;
+for (let i = 0; i < numXTicks; i++) {
+  increment += tickIntervals;
+  xTickValues.push(addDays(startOfToday(), increment));
+}
+
 const legendColors: { [key in string]: string } = {
   exposed: Colors.green,
-  infectious: Colors.lightBlue,
-  hospitalized: Colors.darkRed,
+  infectious: Colors.darkRed,
+  hospitalized: Colors.lightBlue,
   fatalities: Colors.black,
 };
 
@@ -80,7 +97,7 @@ const FacilityProjectionChart: React.FC<ProjectionProps> = ({
     yAccessor: "count",
     responsiveHeight: true,
     responsiveWidth: true,
-    yExtent: [0, maxValue + 100],
+    yExtent: [0, maxValue],
     margin: CHART_MARGINS,
     lineStyle: ({ key }: { key: SeirCompartmentKeys }) => ({
       stroke: legendColors[key],
@@ -91,8 +108,8 @@ const FacilityProjectionChart: React.FC<ProjectionProps> = ({
     axes: [
       {
         orient: "bottom",
-        ticks: 5,
-        tickFormat: (value: Date) => formatDate(new Date(value), "MM/dd"),
+        tickFormat: (value: Date) => formatDate(new Date(value), "M/d"),
+        tickValues: xTickValues,
       },
       {
         orient: "left",
