25f0e80a3ff5c588c2bc8a4dda01d64f00d40713
Ian MacFarland <ian@recidiviz.org>
2020-09-03 18:41:04 -0700

    utility class to handle batch size limits

--- src/database/utils.tsx
+++ src/database/utils.tsx
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 import mapObject from "map-obj";
 
 import { EpidemicModelPersistent } from "../impact-dashboard/EpidemicModelContext";
@@ -25,3 +26,94 @@ export const prepareFromStorage = (state: object): EpidemicModelPersistent => {
     { deep: true },
   );
 };
+
+type BatchOperationType = "CREATE" | "DELETE" | "SET" | "UPDATE";
+type BatchOperationParams = [
+  firebase.firestore.DocumentReference<firebase.firestore.DocumentData>,
+  firebase.firestore.DocumentData,
+  // add other option types when implemented
+  firebase.firestore.SetOptions | undefined,
+];
+type BatchOperation = {
+  type: BatchOperationType;
+  params: BatchOperationParams;
+};
+
+/**
+ * Wrapper around firebase.firestore.WriteBatch that abstracts away the batch size limit.
+ * Exposes methods and properties that would result in batch operations.
+ * Automatically chunks the batch when it's full to prevent overflows.
+ */
+export class BatchWriter {
+  private client: firebase.firestore.Firestore;
+  private currentBatch!: BatchOperation[];
+  // a single "write" can trigger additional operations (in effect,
+  // incrementing the batch size by >1); track them here to anticipate
+  // and prevent overflows
+  private currentBatchSize = 0;
+  // this limit is imposed by Firestore
+  readonly MAX_BATCH_SIZE = 500;
+  private operationBatches: BatchOperation[][] = [];
+  private pendingOpsCount = 0;
+
+  constructor(client: firebase.firestore.Firestore) {
+    this.client = client;
+    this.startBatch();
+  }
+
+  private startBatch() {
+    const newBatch: BatchOperation[] = [];
+    this.currentBatch = newBatch;
+    this.operationBatches.push(newBatch);
+  }
+
+  private trackWrite() {
+    this.currentBatchSize += 1 + this.pendingOpsCount;
+    this.pendingOpsCount = 0;
+  }
+
+  private preventOverflow() {
+    if (this.currentBatchSize + this.pendingOpsCount >= this.MAX_BATCH_SIZE) {
+      this.startBatch();
+    }
+  }
+
+  // NOTE: can also mirror other server transform operations as needed
+  get serverTimestamp() {
+    this.pendingOpsCount += 1;
+    return firebase.firestore.FieldValue.serverTimestamp();
+  }
+
+  // NOTE: can also mirror create, delete, update methods on batch as needed
+  set(
+    ref: BatchOperationParams[0],
+    data: BatchOperationParams[1],
+    options?: firebase.firestore.SetOptions,
+  ) {
+    this.preventOverflow();
+    this.currentBatch.push({
+      type: "SET",
+      params: [ref, data, options],
+    });
+    this.trackWrite();
+    // mimic Firestore interface, allows for chained method calls
+    return this;
+  }
+
+  async commit() {
+    await Promise.all(
+      this.operationBatches.map((operationBatch) => {
+        const writeBatch = this.client.batch();
+        operationBatch.forEach(({ type, params: [ref, data, options] }) => {
+          switch (type) {
+            case "SET":
+              writeBatch.set(ref, data, options);
+              break;
+            // add other methods here as they are implemented
+          }
+        });
+        return writeBatch.commit();
+      }),
+    );
+  }
+}
